56f6e147b3c9fe29af8670692efa3def3c76dd35
MadhuHS <MadhuHS@users.noreply.github.com>
2020-03-26 14:17:13 +0530

    Add files via upload

---
+++ arrays/Program3.java
@@ -0,0 +1,30 @@
+class Program3
+{
+     public static void bubbleSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+        {
+            for (int j = 0; j < n-i-1; j++)
+            {
+                if (arr[j] > arr[j+1])
+                {
+                    // swap temp and arr[i]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+public static void main(String[] args) 
+{
+	int[] a1 = {20,10,30,50,40};
+	bubbleSort(a1);
+}
+}
+
+
+
+
